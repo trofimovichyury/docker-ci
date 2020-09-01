ae3c793384d7755ebeadf1e171cd3f5b1f4fc07e
Yury Trafimovich <yury.trafimovich.extern@joyn.de>
2020-09-01 10:52:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! Something New! And again! And.... again!
         </p>
         <a
           className="App-link"
