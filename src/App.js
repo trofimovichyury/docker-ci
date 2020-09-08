f03fe0993d8bc24216dbd35c3825917bf6f30a47
Yury Trafimovich <yury.trafimovich.extern@joyn.de>
2020-09-08 16:09:42 +0200

    test changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World! Something New! And again! And.... again!
+          Hello World! Something New! And again! ASASSAASASASAnd.... again!
         </p>
         <a
           className="App-link"
